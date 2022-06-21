bc80666a506791b94507daefbc2a2f389e2f7a8d
Jason Mortensen <ingrey1@gmail.com>
2022-06-21 11:13:18 -0700

    add hybrid model table for feedback component blog

---
+++ src/components/ThanksFeedbackModel.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ThanksFeedbackModel() {
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        backgroundColor: "cyan",
+        border: "2px solid black",
+      }}
+    >
+      <p style={{ margin: "5px", textAlign: "center", color: "blue" }}>
+        <b>Thank You for your feedback.</b>
+      </p>
+      <br />
+    </div>
+  );
+}
